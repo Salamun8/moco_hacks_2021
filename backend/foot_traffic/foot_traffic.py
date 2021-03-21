b1b51fd1b3a92127a925fa8de842d0606245d685
GeneralCoder365 <ainesh.chatterjee@gmail.com>
2021-03-20 23:12:41 -0400

    Foot traffic tracker done

--- backend/foot_traffic/foot_traffic.py
+++ backend/foot_traffic/foot_traffic.py
@@ -1,26 +1,33 @@
+# relative imports geocoding.py
 import geocoding as gc
-# import populartimes
-# import livepopulartimes
 import datetime as dt
 import requests
 
+# major values
+# Google API Key for all Google API interactions
 g_api_key = "AIzaSyA592HwCkixwp7W8zRekEf2NZuyfKZNfvc"
+# half mile radius for optimal results for scale within 20 value range
 radius = 0.5
 
-
+# Searches for important buildings nearby, takes in API key, address parameters, and search search radius
 def key_buildings_search(google_api_key, address, city, state, zip_code, search_radius):
     search_results = None
     important_types = ["airport", "amusement_park", "aquarium", "art_gallery", "bank", "casino", "clothing_store", 
     "convenience_store", "department_store", "drugstore", "movie_theater", "museum", "park", "pharmacy", "restaurant", "shopping_mall", "stadium", 
     "store", "subway_station", "supermarket", "tourist_attraction", "zoo", "lodging"]
 
+    # not actually used but helps with visualization of data pipeline
     is_important = False
+    # converts radius from miles to meters
     radius = 1609.34 * search_radius
     base_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
 
+    # gets coordinates for given address
     geo_data = gc.master_geocoding(google_api_key, address, city, state, zip_code)
     latitude = geo_data[0]
     longitude = geo_data[1]
+
+    # sends json request
     endpoint = f"{base_url}?location={latitude}, {longitude}&radius={str(radius)}&opennow&rankby=prominence&key={google_api_key}"
 
     r = requests.get(endpoint)
@@ -28,13 +35,16 @@ def key_buildings_search(google_api_key, address, city, state, zip_code, search_
     if r.status_code not in range(200, 299):
         return None
 
+    # attempts to parse json, and find key locations within 0.5 mile radius, that meet important location types criteria, and gives a total number of them
     try:
+        # sends a json request
         json_file = r.json()
         search_results = json_file["results"]
         raw_number = len(search_results)
         final_number = 0
         status = json_file["status"]
 
+        # logic to get if a place is important or not by iterating through types within each resultant location within 0.5 mile radius
         i = 0
         k = 0
         already_true = False
@@ -43,25 +53,15 @@ def key_buildings_search(google_api_key, address, city, state, zip_code, search_
             already_true = False
             types_length = len(search_results[i]["types"])
             k = 0
-            # while ((k < types_length) and (already_true == False)):
             while ((k < types_length) and (already_true == False)):
                 if(search_results[i]["types"][k] in important_types):
                     is_important = True
                     already_true = True
-                    # final_number = final_number + 1
                 else:
                     is_important = False
-                
-                print(search_results[i]["types"][k])
                 k = k + 1
             if((already_true == True)):
                 final_number = final_number + 1
-            
-            print(search_results[i]["types"])
-            print(is_important)
-            print(already_true)
-            # is_important = False
-            # already_true = False
             i = i + 1
             
 
@@ -69,7 +69,7 @@ def key_buildings_search(google_api_key, address, city, state, zip_code, search_
         # "OK" indicates that no errors occurred; the address was successfully parsed and at least one geocode was returned
         if (status == "OK"):
             # return [final_number, search_results]
-            return [raw_number, final_number]
+            return final_number
         # "ZERO_RESULTS" indicates that the geocode was successful but returned no results. This may occur if the geocoder was passed a non-existent address
         elif (status == "ZERO_RESULTS"):
             return status
@@ -88,17 +88,20 @@ def key_buildings_search(google_api_key, address, city, state, zip_code, search_
         elif (status == "UNKNOWN_ERROR"):
             return status
 
+    # except case
     except:
-        print("Something has gone wrong!")
         # Pass: https://www.google.com/search?q=pass+python+function&rlz=1C1SQJL_enUS806US806&oq=pass+python&aqs=chrome.2.69i57j0l6j69i65.2918j0j1&sourceid=chrome&ie=UTF-8 
         pass
         return None
 
+# converts raw number of important locations to relative scale with weightage based on max important location density and current hour, as a value from 0-100
 def risk_rating_scaler(google_api_key, address, city, state, zip_code, search_radius):
     try:
-        # ONLY BECAUSE KEY_BUILDINGS_SEARCH IS TEMPORARILY RETURNING 2 VALUES!!!
-        raw_value = int(key_buildings_search(google_api_key, address, city, state, zip_code, search_radius)[1])
+        # gets number of important locations
+        raw_value = int(key_buildings_search(google_api_key, address, city, state, zip_code, search_radius))
+        # gets current hour
         current_hour = int(dt.datetime.now().hour)
+        # dictionary of weights from 0.0 to 1.0 based on hour of day on 24 hour system
         time_weights = {
             0: 0.2,
             1: 0.1,
@@ -125,11 +128,16 @@ def risk_rating_scaler(google_api_key, address, city, state, zip_code, search_ra
             22: 0.5,
             23: 0.2
         }
+
+        # max density * most popular time of day to create "total value" for percentage calculation
         max = 20 * 1.0
+        # applies weights to raw number of important locations
         weighted_value = ((raw_value * time_weights[current_hour])/max) * 100
+
         print(raw_value)
-        print("current hour: " + str(current_hour))
         print(weighted_value)
+
+        # assigns scale rating based on weighted_value & returns it
         if(weighted_value < 33.33):
             return "low"
         elif(weighted_value < 66.66):
@@ -138,21 +146,10 @@ def risk_rating_scaler(google_api_key, address, city, state, zip_code, search_ra
             return "high"
         else:
             return "Error"
+
+    # except case
     except:
         return "Error"
 
-
-
-# testing code
-# 10804 Brewer House Road, Rockville, MD 20852
-    # [20,20]
-# 7101 Democracy Blvd, Bethesda, MD 20817
-# 285 Fulton St, New York, NY 10007
-# print(master_at_location(g_api_key, "22 Warren St", "New York", "NY", "10007"))
-print(risk_rating_scaler(g_api_key, "285 BigTitty St", "New York", "NY", "1007", radius))
-# print(key_buildings_search(g_api_key, "10804 Brewer House Road", "Rockville", "MD", "20852", radius))
-
-geo_data = gc.master_geocoding(g_api_key, "285 BigTitty St", "Bew York", "NZ", "10342432007")
-latitude = geo_data[0]
-longitude = geo_data[1]
-print(latitude, longitude)
+# tester code
+print(risk_rating_scaler(g_api_key, "20 W 34th St", "New York", "NY", "10001", radius))
--- backend/foot_traffic/geocoding.py
+++ backend/foot_traffic/geocoding.py
@@ -1,74 +1,68 @@
 import requests
 
-# g_api_key = "AIzaSyA592HwCkixwp7W8zRekEf2NZuyfKZNfvc"
-
+# formats address chunks into whole address
 def address_formatter(address, city, state, zip_code):
     # Ex: "1600 Amphitheatre Pkwy, Mountain View, CA 94043, USA"
     # Make sure state is just 2 letters!
     formatted_address = (address + ", " + city + ", " + state + " " + zip_code + ", USA")
     return formatted_address
 
-# def get_lat_long_locType(input_address):
-def get_lat_long_locType(google_api_key, address, city, state, zip_code):
+# gets latitude, longitude
+def get_lat_long(google_api_key, address, city, state, zip_code):
     input_address = address_formatter(address, city, state, zip_code)
 
     lat = None
     long = None
-    locType = None
     place_id = None
     status = None
     api_key = google_api_key
+
+    # generates json request
     base_url = "https://maps.googleapis.com/maps/api/geocode/json"
     endpoint = f"{base_url}?address={input_address}&key={api_key}"
 
     r = requests.get(endpoint)
 
     if r.status_code not in range(200, 299):
-        return [None, None, None]
+        return [None, None]
     
+    # stores json file and attemps to retrieve latitude and longitude
     try:
         json_file = r.json()
         results = json_file["results"][0]
         lat = results["geometry"]["location"]["lat"]
         long = results["geometry"]["location"]["lng"]
-        # locType = results["geometry"]["location_type"]
-        # Default Place type, since not using Places API
-        locType = "shopping_mall"
-        place_id = results["place_id"]
         status = json_file["status"]
 
         # NON-ERRORS:
         # "OK" indicates that no errors occurred; the address was successfully parsed and at least one geocode was returned
         if (status == "OK"):
-            return [lat, long, locType, place_id]
-        
+            return [lat, long]
+
         # "ZERO_RESULTS" indicates that the geocode was successful but returned no results. This may occur if the geocoder was passed a non-existent address
         elif (status == "ZERO_RESULTS"):
-            return [status, status, status, status]
+            return [status, status]
         
 
         # ERRORS:
         # "REQUEST_DENIED" indicates that your request was denied.
         elif (status == "REQUEST_DENIED"):
-            return [status, status, status, status]
+            return [status, status]
         
         # "INVALID_REQUEST" generally indicates that the query (address, components or latlng) is missing.
         elif (status == "INVALID_REQUEST"):
-            return [status, status, status, status]
+            return [status, status]
         
         # "UNKNOWN_ERROR" indicates that the request could not be processed due to a server error. The request may succeed if you try again.
         elif (status == "UNKNOWN_ERROR"):
-            return [status, status, status, status]
+            return [status, status]
 
+    # except case if json retrieval gives an error
     except:
-        print("Something has gone wrong!")
         # Pass: https://www.google.com/search?q=pass+python+function&rlz=1C1SQJL_enUS806US806&oq=pass+python&aqs=chrome.2.69i57j0l6j69i65.2918j0j1&sourceid=chrome&ie=UTF-8 
         pass
-        return [None, None, None]
+        return [None, None]
 
+# master function
 def master_geocoding(google_api_key, address, city, state, zip_code):
-    return get_lat_long_locType(google_api_key, address, city, state, zip_code)
-
-# Tester Code
-# print(master_geocoding(g_api_key, "10804 Brewer House Road", "Rockville", "MD", "20852"))
-# print(master_geocoding(g_api_key, "6400 Rock Spring Dr", "Bethesda", "MD", "20814"))
+    return get_lat_long(google_api_key, address, city, state, zip_code)
